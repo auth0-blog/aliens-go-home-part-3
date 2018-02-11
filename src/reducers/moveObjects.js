139a9f6f7e8e9310b7d08684d149f095971dc20c
Bruno Krebs <bruno.krebs@auth0.com>
2018-02-11 19:16:10 -0200

    adding algorithm that checks collisions and that remove objects that collide;

--- src/reducers/moveObjects.js
+++ src/reducers/moveObjects.js
@@ -1,6 +1,7 @@
 import { calculateAngle } from '../utils/formulas';
 import createFlyingObjects from './createFlyingObjects';
 import moveBalls from './moveCannonBalls';
+import checkCollisions from './checkCollisions';
 
 function moveObjects(state, action) {
   if (!state.gameState.started) return state;
@@ -15,12 +16,20 @@ function moveObjects(state, action) {
   const newState = createFlyingObjects(state);
 
   const now = (new Date()).getTime();
-  const flyingObjects = newState.gameState.flyingObjects.filter(object => (
+  let flyingObjects = newState.gameState.flyingObjects.filter(object => (
     (now - object.createdAt) < 4000
   ));
 
   const { x, y } = mousePosition;
   const angle = calculateAngle(0, 0, x, y);
+
+  const objectsDestroyed = checkCollisions(cannonBalls, flyingObjects);
+  const cannonBallsDestroyed = objectsDestroyed.map(object => (object.cannonBallId));
+  const flyingDiscsDestroyed = objectsDestroyed.map(object => (object.flyingDiscId));
+
+  cannonBalls = cannonBalls.filter(cannonBall => (cannonBallsDestroyed.indexOf(cannonBall.id)));
+  flyingObjects = flyingObjects.filter(flyingDisc => (flyingDiscsDestroyed.indexOf(flyingDisc.id)));
+
   return {
     ...newState,
     gameState: {
--- src/utils/formulas.js
+++ src/utils/formulas.js
@@ -50,3 +50,8 @@ export const calculateNextPosition = (x, y, angle, divisor = 300) => {
     y: y - stepsY,
   }
 };
+
+export const checkCollision = (rectA, rectB) => (
+  rectA.x1 < rectB.x2 && rectA.x2 > rectB.x1 &&
+  rectA.y1 < rectB.y2 && rectA.y2 > rectB.y1
+);
